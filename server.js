9068799596a1eb732091e58611ece7b6a44e3fc8
Meghana Prasad <142408409+Meghana024@users.noreply.github.com>
2024-08-04 20:52:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+const http=require('http');
+const server=http.createServer((request,response)=>{
+    console.log('request recieved by the server');
+    response.end("this is my first server!");
+    console.log('reponse recived from server');
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
-});
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('server listeniing on port');
+})
